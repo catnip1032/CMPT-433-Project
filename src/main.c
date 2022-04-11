0d7a6956245c799f9b8f67f5991c48a62e58f674
Daniel G. Maia Filho <dg.maia01@gmail.com>
2022-04-10 20:43:35 -0700

    Add code layout

--- src/main.c
+++ src/main.c
@@ -1,38 +1,69 @@
 #include "../include/gate.h"
-#include "../include/pipe.h"
 #include "../include/lights.h"
+#include "../include/pipe.h"
 
-#include <stdio.h>
 #include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main()
+void Main_initialize(void);
+void Main_cleanup(int _singal);
+void Main_setupInterrupt(void);
+
+void Main_stageIdle(void);
+void Main_stageCategorizing(void);
+void Main_stageSorting(void);
+void Main_stageDisposing(void);
+void Main_stageReturning(void);
+
+int main() {}
+
+// Initialization/Termination functions
+// ----------------------------------------------------------------------------
+void Main_initialize(void)
 {
-
-}
-
-void _initialize(void) {
   printf("Initializing recycler.");
 
-  _setupInterrupt();
+  Main_setupInterrupt();
 
-  Gate_init(); 
+  Gate_init();
   Pipe_init();
   Lights_init();
 }
 
-void _terminate(void) {
+void Main_cleanup(int _signal)
+{
   printf("Terminating recycler.");
-  
+
   Gate_cleanup();
   Pipe_cleanup();
   Lights_cleanup();
 }
 
-
-void _setupInterrupt(void) {
+void Main_setupInterrupt(void)
+{
   struct sigaction action;
   struct sigaction oldAction;
   memset(&action, 0, sizeof(action));
-  action.sa_handler = &_terminate;
+  action.sa_handler = &Main_cleanup;
   sigaction(SIGINT, &action, &oldAction);
 }
+
+// Stage functions
+// ----------------------------------------------------------------------------
+
+// TODO: Implement
+void Main_stageIdle(void) {}
+
+// TODO: Implement
+void Main_stageCategorizing(void) {}
+
+// TODO: Implement
+void Main_stageSorting(void) {}
+
+// TODO: Implement
+void Main_stageDisposing(void) {}
+
+// TODO: Implement
+void Main_stageReturning(void) {}
