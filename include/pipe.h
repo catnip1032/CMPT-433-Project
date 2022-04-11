458a475cfa3da53351566edb9f0f129d249c3d8b
Catherine Riopel <criopel@sfu.ca>
2022-04-10 17:13:58 -0700

    Create pipe header

---
+++ include/pipe.h
@@ -0,0 +1,21 @@
+/*
+ * The pipe module acts an abstraction layer for the servo API, sepecified in 
+ * servo.h, so as to manipulate the movement of the TowerPro SG-5010 Servo in
+ * order to physically turn the pipe on the Ball Sorter project.*/
+
+#ifndef _PIPE_HEADER_H_
+#define _PIPE_HEADER_H_
+
+// Initializes the pipe by setting the position of the arm on the TowerPro 
+// SG-5010 to it's initial position.
+void Pipe_init();
+
+// Resets the pipe position by turning the arm attachment on the TowerPro 
+// SG-5010 clockwise until the arm reaches it's initial position.
+void Pipe_resetPipePosition();
+
+// Rotates the pipe position by turning the arm attachment on the TowerPro 
+// SG-5010 counterclockwise to it's most left position.
+void Pipe_rotatePipeToDropBall();
+
+#endif
