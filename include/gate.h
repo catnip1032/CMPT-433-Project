ae8234d0ced235f3b33149d41ff6289abdfc9083
Catherine Riopel <criopel@sfu.ca>
2022-04-10 20:16:16 -0700

    Add values to eGateNum enum and comment cleanup function

--- include/gate.h
+++ include/gate.h
@@ -7,13 +7,15 @@
 #define _GATE_HEADER_H_
 
 // Gate enumerator to specify between gates 1 and gate 2.
-typedef enum { gate1, gate2 } eGateNum;
+typedef enum { gate1 = 1, gate2 = 2 } eGateNum;
 
 // Initialization/Termination functions
 // ----------------------------------------------------------------------------
 // Initializes the gates 1 and 2 by setting the position of the arms on the
 // Micro Servo 98 SG90's to their initial positions where the gates are up.
 void Gate_init(void);
+
+// Sets gate 1 and 2's arms to a resting position where the gate are down.
 void Gate_cleanup(void);
 
 // Behaviour functions
